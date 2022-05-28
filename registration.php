3f5de839e0b347ffa1960be829507574a6022359
Siarhei Uchukhlebau <s.uchuhlebov@mageworx.com>
2022-05-28 20:11:05 +0400

    Initial commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © MageWorx. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MageWorx_ExtendedCartGraphQl', __DIR__);
